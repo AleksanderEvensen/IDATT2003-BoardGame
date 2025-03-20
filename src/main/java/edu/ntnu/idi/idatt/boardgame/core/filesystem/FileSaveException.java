d0fe7b5c950a94031870bdb5e9028415fd0efa54
yazanzarka1 <yazanzarka1@gmail.com>
2025-03-20 10:53:43 +0100

    doc(javadoc): adds more javadoc pre part-2 submit

--- src/main/java/edu/ntnu/idi/idatt/boardgame/core/filesystem/FileSaveException.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/core/filesystem/FileSaveException.java
@@ -1,5 +1,8 @@
 package edu.ntnu.idi.idatt.boardgame.core.filesystem;
 
+/**
+ * An exception that is thrown when a file cannot be deleted.
+ */
 public class FileSaveException extends RuntimeException {
 
   public FileSaveException(String message) {
