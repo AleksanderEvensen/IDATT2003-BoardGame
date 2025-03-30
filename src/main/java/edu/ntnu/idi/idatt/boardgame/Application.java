4c11f2dda32cdeaf637efe9038e48c7467285290
yazanzarka1 <yazanzarka1@gmail.com>
2025-03-30 14:42:31 +0200

    feat(dice): adds a new die component and tests in main.java

--- src/main/java/edu/ntnu/idi/idatt/boardgame/Application.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/Application.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idi.idatt.boardgame;
 
+import edu.ntnu.idi.idatt.boardgame.ui.javafx.components.DieComponent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,9 +9,15 @@ import edu.ntnu.idi.idatt.boardgame.ui.javafx.components.GameBoard;
 import edu.ntnu.idi.idatt.boardgame.core.filesystem.LocalFileProvider;
 import edu.ntnu.idi.idatt.boardgame.game.GameManager;
 import edu.ntnu.idi.idatt.boardgame.model.Player;
+import java.util.Random;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * The main application class.
@@ -35,7 +42,36 @@ public class Application extends javafx.application.Application {
                 .resolveActionStyles()
                 .build();
 
-        Scene scene = new Scene(boardView, 800, 800);
+        DieComponent die = new DieComponent();
+        VBox root = new VBox();
+        root.getChildren().addAll(boardView,die);
+        Random random = new Random();
+        Timeline diceRollTimeline = new Timeline();
+        die.setValue(4);
+
+        for (int i = 0; i < 10; i++) {
+            KeyFrame keyFrame = new KeyFrame(
+                Duration.millis(100 * i),
+                event -> {
+                    int face = random.nextInt(6); // 0 to 5
+                    System.out.println("Rolling die... " + String.valueOf(face + 1));
+
+                    die.setValue(face + 1);
+                }
+            );
+            diceRollTimeline.getKeyFrames().add(keyFrame);
+        }
+        diceRollTimeline.setCycleCount(1);
+
+        Button rollButton = new Button("Roll");
+        rollButton.setOnAction(event -> {
+            diceRollTimeline.play();
+            int face = random.nextInt(6);
+            die.setValue(face + 1);
+        });
+        root.getChildren().add(rollButton);
+
+        Scene scene = new Scene(root, 800, 800);
         scene.getStylesheets().add("main.css");
         stage.setTitle("Hello!");
         stage.setScene(scene);
