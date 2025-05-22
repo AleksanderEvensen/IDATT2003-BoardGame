14c98b005c1809deccc7894d2da5acb174dba5d3
Aleksander Evensen <ahse03@gmail.com>
2025-05-22 10:53:11 +0200

    Fixed complaint

--- src/main/java/edu/ntnu/idi/idatt/boardgame/model/exceptions/BoardRangeException.java
+++ src/main/java/edu/ntnu/idi/idatt/boardgame/model/exceptions/BoardRangeException.java
@@ -1,16 +1,11 @@
 package edu.ntnu.idi.idatt.boardgame.model.exceptions;
 
+import lombok.experimental.StandardException;
+
 /**
  * Exception thrown when a board range is exceeded.
  */
+@StandardException
 public class BoardRangeException extends RuntimeException {
 
-  /**
-   * Constructs a new BoardRangeException with the specified detail message.
-   *
-   * @param message the detail message
-   */
-  public BoardRangeException(String message) {
-    super(message);
-  }
 }
